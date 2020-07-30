8dccc145d8645630f2e5b796a455e0186ce57fe6
liyijue <1754812312@qq.com>
2020-07-31 00:19:11 +0800

    feat(*)初始化css样式 配置element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    <div>hello vue</div>
+    <el-button type="warning">按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  width: 100vw;
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
